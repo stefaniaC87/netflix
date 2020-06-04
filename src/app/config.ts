bf740cc86705919181fb89662334b53e2adfcf2f
Stefania <stefaniacappellino@gmail.com>
2020-06-04 15:19:00 +0200

    modifiche

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export const  CONFIG = {
+  assetsPath: 'assets/',
+  myGithubRepo: 'https://raw.githubusercontent.com/stefaniaC87/netflix/master/src/',
+  hostApi: 'http://netflix.cristiancarrino.com'
+  //hostApi: 'http://localhost/netflix'
+}
+
+
